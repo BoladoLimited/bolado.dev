3670e4555a47d694dc59527ef02217d3ee92e85d
BoladoLimited <74077743+BoladoLimited@users.noreply.github.com>
2021-02-24 21:44:05 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function playMusic() {
-    var player = document.getElementById('player')
-    player.volume = 1.0;
-    if(player.paused) {
-        player.play();
-    }
-}
-
-document.addEventListener('click', playMusic);
